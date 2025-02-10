c9a4fe31f785bcbbb5811f8dd1bafbccf30e1e60
amdreiso <andreiscatolin@gmail.com>
2025-02-09 21:40:27 -0300

    website

---
+++ js/games.js
@@ -0,0 +1,45 @@
+
+let games = [];
+
+function add(img, name, description, url) {
+	games.push({
+		image: "src/img/"+img,
+		name: name,
+		description: description,
+		url: url,
+	});
+}
+
+add("thumbnail1.png", "Space Sim", "Pillage planets, discover galaxies and upgrade your spaceship!", "");
+
+function render() {
+	const gameList = document.getElementById("gameList");
+
+	for (let i = 0; i < games.length; i++) {
+		var anchor = document.createElement("a");
+		anchor.href = games[i].url;
+
+		var container = document.createElement("div");
+		container.className = "gameContainer";
+
+		var image = document.createElement("img");
+		image.className = "gameImage";
+		image.src = games[i].image;
+
+		var title = document.createElement("p");
+		title.className = "gameTitle";
+		title.innerHTML = games[i].name;
+
+		var desc = document.createElement("p");
+		desc.className = "gameDescription";
+		desc.innerHTML = games[i].description;
+
+		container.append(image, title, desc);
+		anchor.append(container);
+
+		gameList.append(anchor);
+	}
+}
+
+render();
+
--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 
+function showSection() {
+  const hash = window.location.hash.substring(1);
+  document.querySelectorAll('.section').forEach(div => {
+    div.style.display = div.id === hash ? 'block' : 'none';
+  });
+}
 
+window.addEventListener('hashchange', showSection);
+window.addEventListener('DOMContentLoaded', showSection);
 
