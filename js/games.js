eafa835c5726c24ecb114f29df2314ff351b2ee5
amdreiso <andreiscatolin@gmail.com>
2025-03-05 00:15:02 -0300

    game pages

--- js/games.js
+++ js/games.js
@@ -1,26 +1,51 @@
 
 let games = [];
 
-function add(img, name, description, url) {
+class PageContents {
+	constructor (description, image, download) {
+		this.description = description;
+		this.image = "src/img/"+image;
+		this.download = download;
+	}
+}
+
+function add(img, name, description, pageContents={}) {
 	games.push({
 		image: "src/img/"+img,
 		name: name,
 		description: description,
-		url: url,
+		pageContents: pageContents,
 	});
 }
 
 // Game list
-add("thumbnail1.png", "Space Sim", "Pillage planets, discover galaxies and upgrade your spaceship!", "");
+add(
+	"thumbnail1.png", 
+	"Space Sim", 
+	"Pillage planets, discover galaxies and upgrade your spaceship!", 
+
+	new PageContents(
+		"Space Sim is a game where you can explore the galaxy, pillage planets and upgrade your spaceship.", 
+		"",
+		""
+	)
+);
 
 
 // Render game links
+const gameDisplayTitle = document.getElementById("gameDisplayTitle");
+const gameDisplayDescription = document.getElementById("gameDisplayDescription");
+
 function render() {
 	const gameList = document.getElementById("gameList");
 
 	for (let i = 0; i < games.length; i++) {
 		var anchor = document.createElement("a");
-		anchor.href = games[i].url;
+		anchor.href = "#gameDisplay";
+		anchor.onclick = function() {
+			gameDisplayTitle.innerHTML = games[i].name;
+			gameDisplayDescription.innerHTML = games[i].pageContents.description;
+		};
 
 		var container = document.createElement("div");
 		container.className = "gameContainer";
