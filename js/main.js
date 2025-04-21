9c9f838e946563efcdc610ede9ce78b1ee2d05ef
amdreiso <andreiscatolin@gmail.com>
2025-04-21 02:02:39 -0300

    nhe

--- js/main.js
+++ js/main.js
@@ -9,3 +9,26 @@ function showSection() {
 window.addEventListener('hashchange', showSection);
 window.addEventListener('DOMContentLoaded', showSection);
 
+
+function getRandomColor() {
+  const max = 60;
+  var r = Math.floor(Math.random() * max);
+  var g = Math.floor(Math.random() * max);
+  var b = Math.floor(Math.random() * max);
+
+  return {
+    r: r,
+    g: g,
+    b: b,
+  }
+}
+
+var sections = document.querySelectorAll("section");
+
+sections.forEach(e => {
+  var color = getRandomColor();
+  //e.style = `background: rgb(${color.r}, ${color.g}, ${color.b});`;
+});
+
+
+
